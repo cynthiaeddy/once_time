1d582ef7f25635d37f8d49daeb0d3d08a7583573
cynthia eddy'git congif -- global user.email cynthiaeddynyc@gmail.comgit config --global user.name cynthia <cynthiaeddynyc@gmail.com>
2021-09-13 17:11:02 -0400

    added wrapper css

--- src/App.js
+++ src/App.js
@@ -2,30 +2,14 @@ import React from 'react'
 import './App.css'
 
 import Home from './components/home/Home'
-import NavBar from './components/navBar/NavBar'
 import Header from './components/Header'
 
 import 'react-responsive-carousel/lib/styles/carousel.min.css'
 
 const App = () => (
-  <div className='container'>
+  <div id='container'>
     <Header />
     <Home />
-    {/* <Route exact path='/about' component={About}/>
-      <Route exact path='/original work' component={Originals}/>
-      <Route exact path='/clothing' component={Fabrics}/>
-      <Route exact path='/print card' component={PrintCard} />
-      <Route exact path='/prints' component={Prints} />
-
-      <Switch>
-        <Route exact path='/prints' render={null} />
-        <Route exact path='/clothing' render={null} />
-        <Route exact path='/about' render={null} />
-        <Route exact path='/original work' render={null} />
-        <Route exact path='/print card' render={null} />
-
-        <Route component={Home} />
-      </Switch> */}
   </div>
 )
 
--- src/components/originals/Originals.js
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-const Originals = () => (
-  <div className='secondary-wrapper'>
-  originals
-  </div>
-
-)
-export default Originals
