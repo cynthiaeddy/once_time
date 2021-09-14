e7c8011ba2663cd5ca773c60e86b4b068b991093
cynthia eddy'git congif -- global user.email cynthiaeddynyc@gmail.comgit config --global user.name cynthia <cynthiaeddynyc@gmail.com>
2021-09-14 15:43:57 -0400

    added fonts

--- src/index.js
+++ src/index.js
@@ -1,12 +1,14 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import {BrowserRouter as Router} from 'react-router-dom'
-import './index.css';
-import App from './App';
+import React from 'react'
+import ReactDOM from 'react-dom'
+import { BrowserRouter as Router } from 'react-router-dom'
+import './index.css'
+import App from './App'
+import './fonts/ACaslonProRegular.ttf'
+import './fonts/SpringvilleRegular3.ttf'
 
 ReactDOM.render(
-      <Router >
-        <App />
-      </Router>,
-    document.getElementById('root')
-);
+  <Router>
+    <App />
+  </Router>,
+  document.getElementById('root')
+)
